d9e828f2ddea3bd9aeb59ff7b3e622c9f4b62550
Grant Lohman <grant_d_lohman@aol.com>
2022-11-08 00:30:31 -0600

    would you rather vue app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+
+      <h1>Would you rather...</h1>
+
+      <!-- gets the template from WouldYouRather.vue -->
+      <would-you-rather v-bind:question="wyrQuestion"
+      v-bind:answer1="wyrAnswer1"
+      v-bind:answer2="wyrAnswer2"
+      v-on:answer-changed="answerChanged"></would-you-rather>
+
+      <p>{{ userSelectionMessage }}</p>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather live in a house shaped like a triangle or a house shaped like a circle?',
+      wyrAnswer1: 'Triangle house',
+      wyrAnswer2: 'Circle house',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks you chose ${choice}`
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background: violet;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2C3E50;
   margin-top: 60px;
+  background: lightskyblue;
 }
 </style>
